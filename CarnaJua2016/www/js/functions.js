563297a3ca47600a3fc18683a43de897242f4acc
washington azevedo <washingtonlsazevedo@hotmail.com>
2016-01-21 01:34:43 -0300

    New pages create

--- CarnaJua2016/www/js/functions.js
+++ CarnaJua2016/www/js/functions.js
@@ -18,8 +18,6 @@ function enviarTexto () {
         "celular": $('#celular').val(),
         "texto": $('#texto').val()
     };
-
-    console.log(JSON.stringify(dados));
     
     $.post(base_url + 'Promocao?securekey=XcQLOs84ZPRhq2te8nMWJMh7RA3wJgkwi08Xh4P6JubYmt2iGC', dados, function (result) {
         alert (JSON.stringify(result));
@@ -28,19 +26,19 @@ function enviarTexto () {
 
 function tempoArtista (idartista) {
     tartista = {
-        1: (new Date(2016, 1, 19, 12, 0, 0, 0)).getTime(),
-        //1: (new Date(2016, 1, 24, 12, 0, 0, 0)).getTime(),
-        19: (new Date(2016, 1, 24, 12, 0, 0, 0)).getTime(),
-        2: (new Date(2016, 1, 22, 12, 0, 0, 0)).getTime(),
-        4: (new Date(2016, 1, 22, 12, 0, 0, 0)).getTime(),
-        7: (new Date(2016, 1, 22, 12, 0, 0, 0)).getTime(),
-        3: (new Date(2016, 1, 23, 12, 0, 0, 0)).getTime(),
-        5: (new Date(2016, 1, 23, 12, 0, 0, 0)).getTime()
+        "1": (new Date(2016, 0, 24, 12, 0, 0, 0)).getTime(),
+        "19": (new Date(2016, 0, 24, 12, 0, 0, 0)).getTime(),
+        "2": (new Date(2016, 0, 22, 12, 0, 0, 0)).getTime(),
+        "4": (new Date(2016, 0, 22, 12, 0, 0, 0)).getTime(),
+        "7": (new Date(2016, 0, 22, 12, 0, 0, 0)).getTime(),
+        "3": (new Date(2016, 0, 23, 12, 0, 0, 0)).getTime(),
+        "5": (new Date(2016, 0, 23, 12, 0, 0, 0)).getTime()
     };
     
     
-    tempo = t[idartista];
+    tempo = tartista[idartista];
     agora = (new Date()).getTime();
     
+
     return agora <= tempo;
 }
