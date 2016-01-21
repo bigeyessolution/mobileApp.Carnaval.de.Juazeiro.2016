a4b4f06b70e5b7aa4ecb908d09468990e40e3c22
Laudivan Freire de Almeida <laudivan@bigeyessolution.com>
2016-01-21 01:15:49 -0300

    Verificando o tempo do artista

--- CarnaJua2016/www/js/functions.js
+++ CarnaJua2016/www/js/functions.js
@@ -1,4 +1,6 @@
 function goartista(idartista) {
+    if ( ! tempoArtista(idartista) ) return;
+    
     $('#fpromocoes #imgartista').attr('src', 'img/' + idartista + '.jpg');
                     
     $('#fpromocoes input').val('');
@@ -22,4 +24,23 @@ function enviarTexto () {
     $.post(base_url + 'Promocao?securekey=XcQLOs84ZPRhq2te8nMWJMh7RA3wJgkwi08Xh4P6JubYmt2iGC', dados, function (result) {
         alert (JSON.stringify(result));
     });
+}
+
+function tempoArtista (idartista) {
+    tartista = {
+        1: (new Date(2016, 1, 19, 12, 0, 0, 0)).getTime(),
+        //1: (new Date(2016, 1, 24, 12, 0, 0, 0)).getTime(),
+        19: (new Date(2016, 1, 24, 12, 0, 0, 0)).getTime(),
+        2: (new Date(2016, 1, 22, 12, 0, 0, 0)).getTime(),
+        4: (new Date(2016, 1, 22, 12, 0, 0, 0)).getTime(),
+        7: (new Date(2016, 1, 22, 12, 0, 0, 0)).getTime(),
+        3: (new Date(2016, 1, 23, 12, 0, 0, 0)).getTime(),
+        5: (new Date(2016, 1, 23, 12, 0, 0, 0)).getTime()
+    };
+    
+    
+    tempo = t[idartista];
+    agora = (new Date()).getTime();
+    
+    return agora <= tempo;
 }
